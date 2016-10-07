15e4e09a0699e97350bae5188d94db0d19b1af6c
Hogan <homyers@student.linnmar.org>
2016-10-07 12:19:08 -0500

    Team 2 commit

--- team2.py
+++ team2.py
@@ -1,4 +1,5 @@
 ####
+#Jack and Hogan
 # Each team's file must define four tokens:
 #     team_name: a string
 #     strategy_name: a string
@@ -6,9 +7,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Something' # Only 10 chars displayed.
+strategy_name = 'Not too sure'
+strategy_description = 'Colludes and betrays'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -25,8 +26,11 @@ def move(my_history, their_history, my_score, their_score):
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
+    if 'b' in their_history:
+        return 'b'
+    else:
+        return 'c'
     
-    return 'c'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
